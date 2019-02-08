8d71c941ce00470a53a6a5cf238ccc8ded920dd7
shreya_shah <shreya.s@tridhya.com>
2019-02-08 10:57:40 +0530

    tesing mail on push

--- app/index.php
+++ app/index.php
@@ -1,1 +1,1 @@
-<?php echo "sucess!! test sdfsfs dfgdfgdg dfdf notification date:7st Feb 2019. test...Testing Demo!!!!!test This is sample PHP using docker git demo!!!"; ?>
+<?php echo "sucess!! notification date:7st Feb 2019. test...Testing Demo!!!!!test This is sample PHP using docker git demo!!!"; ?>
