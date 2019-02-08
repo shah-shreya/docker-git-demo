f8d8a174502002cf398f9ba403bdcffa13c66550
shreya_shah <shreya.s@tridhya.com>
2019-02-08 10:39:25 +0530

    tesing mail on push

--- app/email_notification.php
+++ app/email_notification.php
@@ -2,7 +2,7 @@
  //exec('git rev-parse --verify HEAD 2> /dev/null', $output);
  //$hash = $output[0];
  $message = "test";
- $result = mail("shreya.s@tridhya.com","notification testing","$message");
+ $result = mail("shreya.s@tridhya.com","browser notification testing","$message");
 if(!$result) {   
      echo "Error";   
 } else {
--- app/index.php
+++ app/index.php
@@ -1,1 +1,1 @@
-<?php echo "sucess!! test sdfsfs notification date:7st Feb 2019. test...Testing Demo!!!!!test This is sample PHP using docker git demo!!!"; ?>
+<?php echo "sucess!! test sdfsfs dfgdfgdg notification date:7st Feb 2019. test...Testing Demo!!!!!test This is sample PHP using docker git demo!!!"; ?>
